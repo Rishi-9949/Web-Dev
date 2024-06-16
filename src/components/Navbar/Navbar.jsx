63dda9f254ba90e3ea57a4a1a316973774bfffce
Rishiraj Singh <st22248@ormiston.school.nz>
2024-06-17 11:30:57 +1200

    Navbar Finished
    Navbar Completed With responsiveness, including hamburger menu icon for mobile devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import "./Navbar.scss"
+import { useState } from "react";
 function Navbar() {
+    const [open,setOpen] = useState(false)
     return (
         <nav>
             <div className="left">
@@ -15,6 +17,17 @@ function Navbar() {
             <div className="right">
                 <a href="/">Sign In</a>
                 <div className="btn1">
+                    <a href="/">Sign Up</a>
+                </div>
+                <div className="menuIcon">
+                    <img src="public\menu.png" alt=""onClick={() => setOpen((prev) => !prev)}/>
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Contact Us</a>
+                    <a href="/">Stores</a>
+                    <a href="/">Sign In</a>
                     <a href="/">Sign Up</a>
                 </div>
             </div>
